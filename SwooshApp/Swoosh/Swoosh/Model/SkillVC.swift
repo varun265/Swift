7234c5212d36eadf0857f7a4353cbeceeb134409
Lalaji <varun.varma265@gmail.com>
2018-03-05 11:12:25 +0800

    updating swoosh app

---
+++ SwooshApp/Swoosh/Swoosh/Model/SkillVC.swift
@@ -0,0 +1,39 @@
+//
+//  SkillVC.swift
+//  Swoosh
+//
+//  Created by varun varma on 25/2/18.
+//  Copyright © 2018 varun varma. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onFinishClicked(_ sender: Any) {
+        performSegue(withIdentifier: "ShowStartVCSegue", sender: self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SwooshApp/Swoosh/Swoosh/Model/WelcomeVC.swift
+++ SwooshApp/Swoosh/Swoosh/Model/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
    
     @IBOutlet weak var bgImg: UIImageView!
@@ -25,11 +25,11 @@ class ViewController: UIViewController {
         
     }
 
-    @IBAction func unwindFromSkillVC(unwindSegue : UIStoryboardSegue)
-    {
-            
-    }
     
+    @IBAction func unwindFromLeagueVC(unwindSegue: UIStoryboardSegue)
+    {
+        
+    }
 
 
 }
