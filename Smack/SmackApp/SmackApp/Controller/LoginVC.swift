bad95746e2dcb6b78ba00a41bbb1c8c9848a7691
Lalaji <varun.varma265@gmail.com>
2018-04-14 10:48:59 +0800

    lesson 4

---
+++ Smack/SmackApp/SmackApp/Controller/LoginVC.swift
@@ -0,0 +1,26 @@
+//
+//  LoginVC.swift
+//  SmackApp
+//
+//  Created by varun varma on 14/4/18.
+//  Copyright © 2018 varun varma. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    @IBAction func crossBtnClicked(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    
+
+}
---
+++ Smack/SmackApp/SmackApp/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SmackApp
+//
+//  Created by varun varma on 14/4/18.
+//  Copyright © 2018 varun varma. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
